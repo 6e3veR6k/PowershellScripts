bf57e52cb3a5a1eb00bfae10441632105021bbf7
Безвершук Дмитро Олександрович <bezvershuk_do@or.nt>
2017-08-01 12:02:45 +0300

    Refactore file for work

--- Object_Examples_var2.ps1
+++ Object_Examples_var2.ps1
@@ -1,7 +1,77 @@
-﻿
+﻿class Application 
+{
+
+    [string[]]$Servers;
+    hidden [PSCredential]$AppCredential;
+    hidden [string]$AplicationPath;
+
+    
+    Application([string[]]$servers, [PSCredential]$appCredential, [string]$aplicationPath)
+    {
+        $this.Servers = $servers;
+        $this.AppCredential = $appCredential;
+        $this.AplicationPath = $aplicationPath
+    }
+
+    
+    hidden [string]GetAppFilePath([string]$server)
+    {
+ 
+        $fileInfo = Invoke-Command -ComputerName $server `
+                                    -Credential $this.AppCredential `
+                                    -ScriptBlock { param($Path) Get-ChildItem $Path | where {$_.Name -like "*App_Offline*"} } `
+                                    -ArgumentList $this.AplicationPath;
+        return  $fileInfo.FullName
+    }
+
+
+    
+    hidden [void]RenameFile([string]$NewFileName, [string]$server)
+    {
+ 
+        $fileInfo = Invoke-Command  -ComputerName $server `
+                                    -Credential $this.AppCredential `
+                                    -ScriptBlock { param($App_Offline_FullName, $App_Offline_NewName)  Rename-Item $App_Offline_FullName -NewName $App_Offline_NewName  } `
+                                    -ArgumentList $this.GetAppFilePath($server), $NewFileName;
+        Write-Host "New file name: " $server $this.GetAppFilePath($server)
+    }
+
+    
+    [void]ApplicationClose( [string]$NewFileName ) 
+    {
+        $workApplication = [Application]::new($this.Servers, $this.AppCredential, $this.AplicationPath);
+
+        ForEach ($server in $this.Servers) 
+        {
+            
+            $workApplication.RenameFile($NewFileName, $server)
+        }
+    }
+
+}
+
+
+#Get credential object for our users
 $jAdmin = New-Object System.Management.Automation.PSCredential -ArgumentList 'oranta\jAdmin', (cat '.\jAdminPW.txt' | convertto-securestring);
 $jIncore = New-Object System.Management.Automation.PSCredential -ArgumentList 'oranta\jIncore', (cat '.\jIncore.txt' | convertto-securestring);
 
 
-Invoke-Command -ComputerName 'hq01ju06' -Credential $jAdmin -ScriptBlock {  Get-ChildItem 'C:\inetpub\Jupiter\Amalthee' | where {$_.Name -like "*App_Offline*"} };
+#Describe application servers
+$JupiterServers = [psobject]@{
+    "FrontOffice" = 'hq01ju01'
+    "BackOffice"  = 'hq01ju02', 'hq01ju03', 'hq01ju04', 'hq01ju06', 'hq01ju07'
+    "AmaltheeApp" = 'hq01ju06', 'hq01ju07'
+    "DBServers" = '' 
+}
+
+
+#Define Amalthee application folder:
+$AmaltheePath = 'C:\inetpub\Jupiter\Amalthee';
+
+
+#Get access to application servers
+$AmaltheeApp = [Application]::new($JupiterServers.AmaltheeApp, $jAdmin, $AmaltheePath)
+
+#Change filename on each servers. To stop application change parameter to 'App_Offline.htm'
+$AmaltheeApp.ApplicationClose('____App_Offline.htm')
 
